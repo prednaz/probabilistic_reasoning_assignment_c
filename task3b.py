f2f0c8e85d44935c550df01e8735216ba7fbed0c
rednaZ <p.rednaz@googlemail.com>
2022-10-26 00:11:24 +0200

    why does stan not like this?

--- task3b.py
+++ task3b.py
@@ -25,9 +25,9 @@ def parameters_stan(species_data):
 def model_stan(species_data):
     species = to_identifier_suffix(species_data[0])
     return f"""\
-    temperature{species} ~ normal(a{species} + b{species} * (d18_O{species} - d18_O_w{species}), sigma);
-    a{species} ~ normal(a, sigma_a);
-    b{species} ~ normal(b, sigma_b);
+    temperature{species} ~ normal(sigma_a * a{species} + a + (sigma_b * b{species} + b) * (d18_O{species} - d18_O_w{species}), sigma);
+    a{species} ~ std_normal();
+    b{species} ~ std_normal();
 """
 
 def data_for_stan(species_data):
@@ -72,34 +72,81 @@ fit = posterior.sample(num_chains=2, num_samples=1000)
 
 print(az.summary(fit))
 
+# Messages received during sampling:
+#   Gradient evaluation took 0.000194 seconds
+#   1000 transitions using 10 leapfrog steps per transition would take 1.94 seconds.
+#   Adjust your expectations accordingly!
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
+#   Gradient evaluation took 0.000177 seconds
+#   1000 transitions using 10 leapfrog steps per transition would take 1.77 seconds.
+#   Adjust your expectations accordingly!
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
+#   Informational Message: The current Metropolis proposal is about to be rejected because of the following issue:
+#   Exception: normal_lpdf: Location parameter[1] is inf, but must be finite! (in '/run/user/1000/httpstan_8x23x98b/model_omke7kqd.stan', line 83, column 4 to column 194)
+#   If this warning occurs sporadically, such as for highly constrained variable types like covariance matrices, then the sampler is fine,
+#   but if this warning occurs often then your model may be either severely ill-conditioned or misspecified.
 #                                 mean     sd  hdi_3%  hdi_97%  mcse_mean  mcse_sd  ess_bulk  ess_tail  r_hat
-# sigma                          0.837  0.033   0.781    0.901      0.001    0.000    2275.0    1420.0   1.00
-# a                             15.500  0.935  13.782   17.216      0.019    0.014    2671.0    1640.0   1.00
-# b                             -3.804  0.258  -4.303   -3.337      0.006    0.004    2176.0    1549.0   1.00
-# sigma_a                        2.934  0.743   1.717    4.309      0.017    0.013    2043.0    1610.0   1.00
-# sigma_b                        0.703  0.235   0.289    1.122      0.007    0.005    1128.0    1122.0   1.00
-# a_cibicides_pachyderma        14.473  0.187  14.120   14.820      0.004    0.003    2729.0    1404.0   1.00
-# b_cibicides_pachyderma        -4.364  0.181  -4.722   -4.047      0.004    0.003    2417.0    1362.0   1.00
-# a_cibicidoides_wuellerstorfi   9.896  0.671   8.693   11.237      0.016    0.011    1842.0    1318.0   1.00
-# b_cibicidoides_wuellerstorfi  -2.994  0.229  -3.441   -2.574      0.005    0.004    1828.0    1347.0   1.00
-# a_globorotalia_menardii       17.722  0.880  16.144   19.452      0.023    0.016    1571.0     976.0   1.00
-# b_globorotalia_menardii       -3.253  0.287  -3.769   -2.703      0.007    0.005    1608.0    1078.0   1.00
-# a_hoeglundina_elegans         18.728  0.135  18.479   18.988      0.003    0.002    2548.0    1694.0   1.00
-# b_hoeglundina_elegans         -3.948  0.052  -4.051   -3.855      0.001    0.001    2574.0    1527.0   1.00
-# a_neogloboquadrina_dutertrei  18.098  1.117  15.968   20.160      0.026    0.018    1882.0    1399.0   1.00
-# b_neogloboquadrina_dutertrei  -3.300  0.357  -3.985   -2.642      0.008    0.006    1870.0    1264.0   1.00
-# a_orbulina_universa           14.278  0.973  12.548   16.128      0.022    0.016    1958.0    1302.0   1.00
-# b_orbulina_universa           -4.509  0.371  -5.241   -3.874      0.009    0.006    1847.0    1263.0   1.00
-# a_planulina_ariminensis       13.687  0.641  12.489   14.828      0.013    0.009    2386.0    1287.0   1.00
-# b_planulina_ariminensis       -3.850  0.534  -4.792   -2.798      0.011    0.008    2193.0    1429.0   1.00
-# a_planulina_foveolata         13.798  0.264  13.310   14.284      0.005    0.004    2620.0    1579.0   1.01
-# b_planulina_foveolata         -4.309  0.408  -5.096   -3.581      0.009    0.006    2326.0    1634.0   1.00
-# a_uvigerina_curticosta        17.098  1.645  13.862   20.080      0.040    0.029    1730.0    1149.0   1.00
-# b_uvigerina_curticosta        -4.106  0.548  -5.216   -3.142      0.013    0.009    1777.0    1176.0   1.00
-# a_uvigerina_flintii           16.376  0.344  15.732   17.020      0.006    0.004    3099.0    1612.0   1.00
-# b_uvigerina_flintii           -3.346  0.490  -4.298   -2.486      0.009    0.007    2788.0    1591.0   1.00
-# a_uvigerina_peregrina         16.223  0.297  15.658   16.766      0.006    0.004    2435.0    1530.0   1.00
-# b_uvigerina_peregrina         -4.031  0.159  -4.317   -3.727      0.003    0.002    2379.0    1350.0   1.00
+# sigma                          0.838  0.031   0.780    0.897      0.001    0.000    2764.0    1314.0    1.0
+# a                             15.476  0.915  13.696   17.242      0.037    0.026     614.0     812.0    1.0
+# b                             -3.822  0.249  -4.262   -3.334      0.009    0.007     723.0     827.0    1.0
+# sigma_a                        2.930  0.798   1.619    4.329      0.031    0.022     670.0     806.0    1.0
+# sigma_b                        0.678  0.224   0.311    1.101      0.008    0.006     722.0    1156.0    1.0
+# a_cibicides_pachyderma        -0.365  0.325  -0.972    0.256      0.013    0.009     668.0     857.0    1.0
+# b_cibicides_pachyderma        -0.858  0.501  -1.822    0.020      0.016    0.012     929.0    1024.0    1.0
+# a_cibicidoides_wuellerstorfi  -2.002  0.567  -3.124   -1.021      0.020    0.014     778.0    1054.0    1.0
+# b_cibicidoides_wuellerstorfi   1.290  0.546   0.246    2.277      0.018    0.012     960.0    1274.0    1.0
+# a_globorotalia_menardii        0.791  0.450  -0.022    1.596      0.015    0.010     946.0    1320.0    1.0
+# b_globorotalia_menardii        0.862  0.560  -0.175    1.932      0.016    0.012    1177.0    1483.0    1.0
+# a_hoeglundina_elegans          1.187  0.432   0.349    1.938      0.017    0.012     666.0     698.0    1.0
+# b_hoeglundina_elegans         -0.207  0.373  -0.866    0.490      0.014    0.010     751.0     894.0    1.0
+# a_neogloboquadrina_dutertrei   0.922  0.490   0.054    1.879      0.015    0.011    1070.0    1405.0    1.0
+# b_neogloboquadrina_dutertrei   0.784  0.606  -0.327    1.958      0.016    0.012    1364.0    1414.0    1.0
+# a_orbulina_universa           -0.409  0.466  -1.273    0.456      0.015    0.011     961.0    1221.0    1.0
+# b_orbulina_universa           -1.038  0.635  -2.191    0.183      0.016    0.012    1476.0    1342.0    1.0
+# a_planulina_ariminensis       -0.638  0.388  -1.416    0.057      0.014    0.010     778.0     786.0    1.0
+# b_planulina_ariminensis       -0.066  0.776  -1.525    1.293      0.016    0.016    2218.0    1607.0    1.0
+# a_planulina_foveolata         -0.608  0.348  -1.301    0.030      0.013    0.009     693.0     956.0    1.0
+# b_planulina_foveolata         -0.684  0.673  -1.962    0.537      0.016    0.014    1844.0    1343.0    1.0
+# a_uvigerina_curticosta         0.569  0.589  -0.459    1.711      0.017    0.013    1191.0    1108.0    1.0
+# b_uvigerina_curticosta        -0.419  0.775  -1.899    0.964      0.021    0.016    1307.0    1344.0    1.0
+# a_uvigerina_flintii            0.328  0.334  -0.277    0.958      0.013    0.009     686.0    1104.0    1.0
+# b_uvigerina_flintii            0.663  0.730  -0.666    2.055      0.016    0.013    2128.0    1319.0    1.0
+# a_uvigerina_peregrina          0.274  0.331  -0.320    0.921      0.013    0.009     697.0    1081.0    1.0
+# b_uvigerina_peregrina         -0.329  0.433  -1.150    0.438      0.014    0.010     931.0    1200.0    1.0
 
 # Gelman-Rubin statistic \texttt{r_hat} within 0.05 of 1 indicates that the
 # chain has converged and therefore the sample is drawn from the
