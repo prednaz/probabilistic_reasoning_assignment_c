d6cd31aff45a696ad39c7466c866b074130b65ff
rednaZ <p.rednaz@googlemail.com>
2022-10-28 00:13:10 +0200

    bug

--- task3b.py
+++ task3b.py
@@ -4,38 +4,7 @@ from operator import itemgetter
 import pandas as pd
 import stan
 
-data = pd.read_csv("merged_data.csv")[["species", "temperature", "d18_O_w", "d18_O"]].groupby("species")
-
-def data_stan(species_data):
-    species = to_identifier_suffix(species_data[0])
-    return f"""\
-    int<lower=0> N{species};
-    vector[N{species}] temperature{species};
-    vector[N{species}] d18_O_w{species};
-    vector[N{species}] d18_O{species};
-"""
-
-def parameters_stan(species_data):
-    species = to_identifier_suffix(species_data[0])
-    return f"""\
-    real a_raw{species};
-    real b_raw{species};
-"""
-
-def transformed_parameters_stan(species_data):
-    species = to_identifier_suffix(species_data[0])
-    return f"""\
-    real a{species} = sigma_a * a_raw{species} + a;
-    real b{species} = sigma_b * b_raw{species} + b;
-"""
-
-def model_stan(species_data):
-    species = to_identifier_suffix(species_data[0])
-    return f"""\
-    temperature{species} ~ normal(a{species} + b{species} * (d18_O{species} - d18_O_w{species}), sigma);
-    a_raw{species} ~ std_normal();
-    b_raw{species} ~ std_normal();
-"""
+data = pd.read_csv("merged_data.csv")[:1][["species", "temperature", "d18_O_w", "d18_O"]].groupby("species")
 
 def data_for_stan(species_data):
     species = to_identifier_suffix(species_data[0])
@@ -50,25 +19,33 @@ for data_new in map(data_for_stan, data):
     data_current.update(data_new)
 
 posterior = stan.build(
-f"""\
-data {{
-{"".join(map(data_stan, data))}}}
-parameters {{
+"""\
+data {
+    int<lower=0> N_hoeglundina_elegans;
+    vector[N_hoeglundina_elegans] temperature_hoeglundina_elegans;
+    vector[N_hoeglundina_elegans] d18_O_w_hoeglundina_elegans;
+    vector[N_hoeglundina_elegans] d18_O_hoeglundina_elegans;
+}
+parameters {
     real<lower=0> sigma;
     real a;
     real b;
     real<lower=0> sigma_a;
     real<lower=0> sigma_b;
-{"".join(map(parameters_stan, data))}}}
-transformed parameters {{
-{"".join(map(transformed_parameters_stan, data))}}}
-model {{
+    real a_hoeglundina_elegans;
+    real b_hoeglundina_elegans;
+}
+model {
+    print(sigma_a);
     sigma ~ normal(0, 2);
     a ~ normal(24, 26);
     b ~ normal(-3.5, 13.5);
     sigma_a ~ normal(0, 4);
     sigma_b ~ normal(0, 2);
-{"".join(map(model_stan, data))}}}
+    temperature_hoeglundina_elegans ~ normal(sigma_a * a_hoeglundina_elegans + a + (sigma_b * b_hoeglundina_elegans + b) * (d18_O_hoeglundina_elegans - d18_O_w_hoeglundina_elegans), sigma);
+    a_hoeglundina_elegans ~ std_normal();
+    b_hoeglundina_elegans ~ std_normal();
+}
 """,
     data=data_current,
     random_seed=1,
